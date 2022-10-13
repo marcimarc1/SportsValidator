8ec4af44bc8793ddd458e383d106f82607d1beaa
Michael Altmann <michael.altmann@tum.de>
2022-10-14 00:00:37 +0200

    continueing with File List, loading file information from JSON file, adding metadata and changeable name; added thumbnail image file

--- frontend/src/components/pages/FileOverview/FileListItem.js
+++ frontend/src/components/pages/FileOverview/FileListItem.js
@@ -1,18 +1,64 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faTrash, faPenToSquare, faChartColumn} from '@fortawesome/free-solid-svg-icons'
 import {Card} from "@material-ui/core";
+import { EditText, EditTextarea } from 'react-edit-text';
+import 'react-edit-text/dist/index.css';
+
 import Thumbnail1 from "../../../data/thumbnail_1.jpg";//"../../../data/Thumbnail_1.jpg"
 import "./FileOverview.css"
 
 class FileListItem extends Component {
-    render() {
-        return (
-            <div className="FileOverviewFileListItem">
-                <img className="FileOverviewFileListItemThumbnail" src={Thumbnail1} alt="Thumbnail">
 
-                </img>
+    // image dimensions of original image, need to be changed when changing resolution of thumbnail!
+    thumbnailImageWidth = 500;
+    thumbnailImageHeight = 281;
+
+    thumbnailRescale = 0.5;
+
+    handleChangeName = (obj) => {
+        this.props.changeName(this.props.id, obj.value);
+    }
+
+    render() {
+        let thumbnailWidth = Math.floor(this.thumbnailImageWidth * this.thumbnailRescale);
+        let thumbnailHeight = Math.floor(this.thumbnailImageHeight * this.thumbnailRescale);
+        let duration = this.props.duration;
+        if(duration < 60)
+            duration = duration.toString() + "min";
+        else
+            duration = `${Math.floor(duration / 60)}h ${duration % 60}min`;
+        return (
+            <div className="FileOverviewListItem" style={{height: thumbnailHeight}}>
+                <img className="FileOverviewListItemThumbnail" src={Thumbnail1} alt="Thumbnail" width={thumbnailWidth} height={thumbnailHeight}/>
+                <div className="FileOverviewListItemMetadata">
+                    <EditText
+                        className="FileOverviewListItemMetadataName"
+                        defaultValue={this.props.name}
+                        onSave={this.handleChangeName}
+                    />
+                    <div className="Duration">
+                        <p>{'  '}Duration: {duration}</p>
+                    </div>
+                </div>
+                <div className="FileOverviewListItemButtons">
+                    <FontAwesomeIcon icon={faPenToSquare} />
+                    <FontAwesomeIcon icon={faTrash} />
+                    <FontAwesomeIcon icon={faChartColumn} />
+
+                </div>
+
             </div>
         );
     }
+}
+
+FileListItem.propTypes = {
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    duration: PropTypes.number.isRequired,      //video duration in minutes
+    changeName: PropTypes.func.isRequired
 }
 
 export default FileListItem;
--- frontend/src/components/pages/FileOverview/FileOverview.js
+++ frontend/src/components/pages/FileOverview/FileOverview.js
@@ -2,13 +2,46 @@ import React, {Component} from 'react';
 import FileListItem from "./FileListItem";
 import "./FileOverview.css"
 
+import videofiles from "../../../data/videofiles.json";
+
+
 class FileOverview extends Component {
+
+    state = {
+        fileListItems: []
+    }
+
+    componentDidMount() {
+        let files = videofiles.files;
+        console.log(files);
+        let fileListItems = [];
+        for(const f of files) {
+            console.log(f);
+            fileListItems[f.id] = {name: f.name, duration: f.duration};
+        }
+        this.setState({fileListItems});
+    }
+
+    changeName(id, name) {
+        // TODO BACKEND
+        // BACKEND.changeName(id, name)
+        this.fileListItems[id].name = name;
+    }
+
     render() {
+        let fileListItemComponents = this.state.fileListItems.map((e, id) => <FileListItem id={id} name={e.name} duration={e.duration} changeName={this.changeName} />);
+        // let test = this.fileListItems[1];
+        // console.log("****************");
+        // console.log(test);
+        // test = <FileListItem id={1} name={test.name} duration={test.duration} changeName={this.changeName} />;
+
         return (
             <div className="FileOverview">
-                File Overview
-                <FileListItem/>
-                <FileListItem/>
+                <h1 className={"FileOverviewHeading"}>File Overview</h1>
+                <div className="FileOverviewList">
+
+                    {fileListItemComponents}
+                </div>
             </div>
         );
     }
