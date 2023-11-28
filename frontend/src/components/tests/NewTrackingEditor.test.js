ddeb50a7ca61e1e93ba2388540622fd8029d47fe
Yiming Chen <ge83gix@tum.de>
2023-11-28 23:21:55 +0100

    add dependencies for testing
    Former-commit-id: faf3515341276bc79c39ddf0036c5cc27fd2b5f8

---
+++ frontend/src/components/tests/NewTrackingEditor.test.js
@@ -0,0 +1,12 @@
+import { fabric } from 'fabric';
+
+const editor = require('../pages/newTrackingEditor/NewTrackingEditor');
+
+test('update player name', () => {
+    //given
+    const playerBox = new fabric.Rect();
+
+    //when
+
+    //then
+}); 
