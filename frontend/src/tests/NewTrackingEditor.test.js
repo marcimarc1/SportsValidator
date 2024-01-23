cd1d6ff518dbcd6920f452230682c8999539fdc4
ge83gix <schalker@Edgars-Computer>
2024-01-23 16:34:09 +0100

    test class to check player number after adding a player
    Former-commit-id: 081307070da9d86aff8451d54040687de1770871

--- frontend/src/tests/NewTrackingEditor.test.js
+++ frontend/src/tests/NewTrackingEditor.test.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import {render} from "@testing-library/react"
+import {fireEvent, getNodeText, screen, render, cleanup} from "@testing-library/react"
 import { fabric } from 'fabric';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Header from "../components/layout/Header";
+import '@testing-library/jest-dom/extend-expect'
 import NewTrackingEditor from '../components/pages/newTrackingEditor/NewTrackingEditor'
 
+afterEach(cleanup);
 
-it('new player is displayed after added', () => {
-    const {queryByLabelText, getByLabelText} = render(
-        <NewTrackingEditor/>,
-    );
+describe('add player button', () => {
+
+    it('increases number of players by 1', () => {
+        render(<NewTrackingEditor/>);
+    
+        //given
+        const number = screen.getByTestId('player-number');
+        const button = screen.getByTestId('add-player-button');
+        const initialText = getNodeText(number);
+        expect(initialText).toEqual("player number: 0");
+    
+        //when
+        fireEvent.click(button);
+    
+        //then
+        const updatedText = getNodeText(number);
+        expect(updatedText).toEqual("player number: 1");
+    });
+
+
 });
