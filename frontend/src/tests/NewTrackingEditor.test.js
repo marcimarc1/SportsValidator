c65ff495f4124a3592210eccbd293b160c520aca
atalayozmen <atalayozmen21@gmail.com>
2024-04-10 21:12:02 +0200

    non working test

--- frontend/src/tests/NewTrackingEditor.test.js
+++ frontend/src/tests/NewTrackingEditor.test.js
@@ -165,3 +165,70 @@ describe("player sidebar", () => {
     ).toEqual("player2");
   });
 });
+
+// Import the 'fs' module for file system operations
+import { readFileSync } from 'fs';
+
+import path from 'path'
+
+
+describe("multi select merging works correct", () => {
+  it("merges every selected player into one player key, which has the smallest frame number", async () => {
+
+    // Specify the path to your file
+    const filePath = path.resolve(__dirname, './test_csvs/test.csv');
+
+    let fileContent;
+    // Read file content synchronously
+    try {
+      fileContent = readFileSync(filePath, 'utf-8');
+    } catch (error) {
+      console.error('Error reading the file:', error);
+    }
+
+    //given
+    useLocation.mockReturnValue({
+      state: {
+        processedPlayers: fileContent
+      },
+    });
+
+    const logSpy = jest.spyOn(global.console, "log");
+    const mockAnnotation = parseProcessedPlayers(mockCSV);
+
+    //when
+    render(<NewTrackingEditor />);
+
+    //then
+    await waitFor(() => {
+      expect(logSpy).toHaveBeenCalledWith(
+        "retrieved annotation:",
+        mockAnnotation,
+      );
+    });
+
+    const nextFrameButton = screen.getByTestId("next-frame-button");
+    
+    
+    //click it 332 times
+
+    for (let i = 0; i < 332; i++) {
+      fireEvent.click(nextFrameButton);
+    }
+
+    //wait a bit
+    await new Promise(r => setTimeout(r, 1000));
+    const canvasElement = screen.getByTestId("fabric-canvas");
+    var JSONCanvas = canvasElement.getAttribute("canvas");
+    const initialCanvas = new fabric.Canvas("canvas");
+    //retrieve serialized fabric canvas
+    initialCanvas.loadFromJSON(
+      JSONCanvas,
+      initialCanvas.renderAll.bind(initialCanvas),
+    );
+    console.log(initialCanvas.getObjects().length);
+
+    const playerList = container.querySelector(".TrackListList").children[0];
+    console.log(playerList.childElementCount);
+  });
+});
