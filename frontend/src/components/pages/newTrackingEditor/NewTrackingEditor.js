904cc3047df32d46c2159bc94900be071e434370
Atalay Özmen <atalayozmen21@gmail.com>
2024-04-24 13:34:31 +0200

    Disable trailing points outside the track (#56)
    * in field check for trails and refactor of loops
    
    * fix field issue in trail redraw
    
    * reformat

--- frontend/src/components/pages/newTrackingEditor/NewTrackingEditor.js
+++ frontend/src/components/pages/newTrackingEditor/NewTrackingEditor.js
@@ -442,7 +442,6 @@ const NewTrackingEditor = () => {
   const drawBoundingBoxes = (frameNumber) => {
     const horizontalScalingFactor = canvas.width / 3840;
     const verticalScalingFactor = canvas.height / 2160;
-    let playerIndex = 0;
     deselectAllBox();
 
     if (!trailsEnabled) {
@@ -463,7 +462,7 @@ const NewTrackingEditor = () => {
           ),
       );
       const currentTrailsToDraw = annotations.filter(
-        (a) => a.FrameNo == frameNumber,
+        (a) => a.FrameNo == frameNumber && (a.in_field || a.in_field === null),
       );
 
       currentTrailsToDraw.forEach((a) => {
@@ -489,23 +488,23 @@ const NewTrackingEditor = () => {
       });
     }
 
-    var playersToDraw = canvasBoxes.filter(
+    var boundingBoxesToDraw = canvasBoxes.filter(
       (a) =>
         a.my.frame == frameNumber && (a.my.in_field || a.my.in_field === null),
     );
     var tempList = [];
     let runningIndex = 0;
 
-    if (playersToDraw.length > 0) {
-      while (playerIndex < playersToDraw.length) {
-        canvas.add(playersToDraw[playerIndex]);
-        const boxColor = colorSet.get(playersToDraw[playerIndex].my.key); //used in 'stroke' property of playerBox
+    if (boundingBoxesToDraw.length > 0) {
+      boundingBoxesToDraw.forEach((boundingBox) => {
+        canvas.add(boundingBox);
+        const boxColor = colorSet.get(boundingBox.my.key); //used in 'stroke' property of playerBox
 
         tempList = tempList.concat([
           <TrackListItemPlayer
             key={runningIndex++}
-            playerBox={playersToDraw[playerIndex]}
-            name={playerNameMap.get(playersToDraw[playerIndex].my.key)}
+            playerBox={boundingBox}
+            name={playerNameMap.get(boundingBox.my.key)}
             changeSelection={changeSelection}
             setName={setName}
             blink={blink}
@@ -514,24 +513,20 @@ const NewTrackingEditor = () => {
             handleModalOpen={handleModalOpen}
           />,
         ]);
-        playerIndex++;
-      }
+      });
     } else {
-      playersToDraw = annotations.filter(
+      boundingBoxesToDraw = annotations.filter(
         (a) => a.FrameNo == frameNumber && (a.in_field || a.in_field === null),
       );
 
-      if (playersToDraw.length > 0) {
+      if (boundingBoxesToDraw.length > 0) {
         //draw boxes
-        while (playerIndex < playersToDraw.length) {
-          const scaledX =
-            playersToDraw[playerIndex].x1 * horizontalScalingFactor;
-          const scaledY = playersToDraw[playerIndex].y1 * verticalScalingFactor;
-          const scaledWidth =
-            playersToDraw[playerIndex].w * horizontalScalingFactor;
-          const scaledHeight =
-            playersToDraw[playerIndex].h * verticalScalingFactor;
-          const boxColor = colorSet.get(playersToDraw[playerIndex].PlayerKey); //used in 'stroke' property of playerBox
+        boundingBoxesToDraw.forEach((boundingBox) => {
+          const scaledX = boundingBox.x1 * horizontalScalingFactor;
+          const scaledY = boundingBox.y1 * verticalScalingFactor;
+          const scaledWidth = boundingBox.w * horizontalScalingFactor;
+          const scaledHeight = boundingBox.h * verticalScalingFactor;
+          const boxColor = colorSet.get(boundingBox.PlayerKey); //used in 'stroke' property of playerBox
           let playerBox = new fabric.Rect({
             left: scaledX,
             top: scaledY,
@@ -552,7 +547,7 @@ const NewTrackingEditor = () => {
           });
           playerBox.my = {
             selected: false,
-            key: playersToDraw[playerIndex].PlayerKey,
+            key: boundingBox.PlayerKey,
             frame: frameNumber,
             //scaling factor when modifying the box
             scaleX: 1,
@@ -569,7 +564,7 @@ const NewTrackingEditor = () => {
               key={runningIndex++}
               playerBox={playerBox}
               color={boxColor}
-              name={playerNameMap.get(playersToDraw[playerIndex].PlayerKey)}
+              name={playerNameMap.get(boundingBox.PlayerKey)}
               changeSelection={changeSelection}
               setName={setName}
               blink={blink}
@@ -577,8 +572,7 @@ const NewTrackingEditor = () => {
               handleModalOpen={handleModalOpen}
             />,
           ]);
-          playerIndex++;
-        }
+        });
       }
     }
     setPlayerList(tempList);
@@ -736,20 +730,17 @@ const NewTrackingEditor = () => {
 
     //same thing we do in drawBoundingBoxes..
     if (annotations.length > 0) {
-      var playersToDraw = annotations.filter(
+      var boundingBoxesToDraw = annotations.filter(
         (a) => a.FrameNo == frameNumber && (a.in_field || a.in_field === null),
       );
-      if (playersToDraw.length > 0) {
+      if (boundingBoxesToDraw.length > 0) {
         //draw boxes
-        while (playerIndex < playersToDraw.length) {
-          const scaledX =
-            playersToDraw[playerIndex].x1 * horizontalScalingFactor;
-          const scaledY = playersToDraw[playerIndex].y1 * verticalScalingFactor;
-          const scaledWidth =
-            playersToDraw[playerIndex].w * horizontalScalingFactor;
-          const scaledHeight =
-            playersToDraw[playerIndex].h * verticalScalingFactor;
-          const boxColor = colorSet.get(playersToDraw[playerIndex].PlayerKey); //used in 'stroke' property of playerBox
+        boundingBoxesToDraw.forEach((boundingBox) => {
+          const scaledX = boundingBox.x1 * horizontalScalingFactor;
+          const scaledY = boundingBox.y1 * verticalScalingFactor;
+          const scaledWidth = boundingBox.w * horizontalScalingFactor;
+          const scaledHeight = boundingBox.h * verticalScalingFactor;
+          const boxColor = colorSet.get(boundingBox.PlayerKey); //used in 'stroke' property of playerBox
 
           let playerBox = new fabric.Rect({
             left: scaledX,
@@ -769,7 +760,7 @@ const NewTrackingEditor = () => {
           });
           playerBox.my = {
             selected: false,
-            key: playersToDraw[playerIndex].PlayerKey,
+            key: boundingBox.PlayerKey,
             frame: frameNumber,
             //scaling factor when modifying the box
             scaleX: 1,
@@ -785,7 +776,7 @@ const NewTrackingEditor = () => {
             <TrackListItemPlayer
               key={runningIndex++}
               playerBox={playerBox}
-              name={playerNameMap.get(playersToDraw[playerIndex].PlayerKey)}
+              name={playerNameMap.get(boundingBox.PlayerKey)}
               changeSelection={changeSelection}
               setName={setName}
               blink={blink}
@@ -794,8 +785,7 @@ const NewTrackingEditor = () => {
               handleModalOpen={handleModalOpen}
             />,
           ]);
-          playerIndex++;
-        }
+        });
         setPlayerList(tempList);
       }
     }
