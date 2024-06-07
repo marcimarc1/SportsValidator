09218dd234c854e803eb9a1af909c7b1bbe02371
Tobias Maierhofer <tobias.maierhofer@unibw.de>
2024-06-07 11:20:19 +0200

    adjust ballbox size

--- frontend/src/utils/AnnotationBoxConverter.js
+++ frontend/src/utils/AnnotationBoxConverter.js
@@ -74,8 +74,8 @@ export const convertBallsAnnotationToBox = (
   try {
     const scaledX = ballinfo.x1 * horizontalScalingFactor;
     const scaledY = ballinfo.y1 * verticalScalingFactor;
-    const scaledWidth = 20 * horizontalScalingFactor;
-    const scaledHeight = 20 * verticalScalingFactor;
+    const scaledWidth = 15 * horizontalScalingFactor;
+    const scaledHeight = 15 * verticalScalingFactor;
     const boundingBox = new fabric.Rect({
       left: scaledX,
       top: scaledY,
