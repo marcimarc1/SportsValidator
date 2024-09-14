7b69022f85e91504704a2171337a6964de72ddfd
MarcoMenzel7 <33926409+MarcoMenzel7@users.noreply.github.com>
2024-09-14 13:25:40 +0200

    72 Player visualisation on field  (#74)
    * #72 Player visualisation on the field
    
    * #72 fix prettier issues
    
    ---------
    
    Co-authored-by: Marco Menzel <ga58vaf@ForStudents.onmicrosoft.com>
    Co-authored-by: marcomenzel <ga58vaf@mytum.de>

--- frontend/src/components/pages/newTrackingEditor/NewTrackingEditor.js
+++ frontend/src/components/pages/newTrackingEditor/NewTrackingEditor.js
@@ -591,6 +591,22 @@ const NewTrackingEditor = () => {
         });
       }
     }
+
+    if (isShowingBox) {
+      boundingBoxesToDraw.forEach((a) => {
+        const fontSize = 12;
+        const scaledX = a.x1 * horizontalScalingFactor;
+        const scaledY = a.y1 * verticalScalingFactor - fontSize;
+        const playerKey = a.PlayerKey.toString();
+        let boxKey = new fabric.Text(playerKey, {
+          left: scaledX,
+          top: scaledY,
+          fontSize: fontSize,
+        });
+        canvas.add(boxKey);
+      });
+    }
+
     setPlayerList(tempList);
     canvas.renderAll();
   };
