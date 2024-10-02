fe0f1afd1804cbfc8f8b0a017c4024e648900e73
mehmet <mehmet@wagmitech.co>
2024-10-02 18:20:36 +0200

    fix: dont remove field objects on showInField update

--- frontend/src/components/pages/newTrackingEditor/NewTrackingEditor.js
+++ frontend/src/components/pages/newTrackingEditor/NewTrackingEditor.js
@@ -601,7 +601,7 @@ const NewTrackingEditor = () => {
     }
 
     canvas.remove(
-      ...canvas.getObjects().filter((obj) => obj.properties?.type === "field"),
+      ...canvas.getObjects().filter((obj) => obj.properties?.type === "field" || obj.properties?.type === "fieldPoint"),
     );
 
     if (showField) {
@@ -756,7 +756,7 @@ const NewTrackingEditor = () => {
   //triggered when new player is added, or when merge or swap happens
   useEffect(() => {
     //remove old canvas objects
-    canvas.remove(...canvas.getObjects());
+    canvas.remove(...canvas.getObjects().filter((obj) => obj?.properties?.type !== "fieldPoint" && obj?.properties?.type !== "field"));
 
     const horizontalScalingFactor = canvas.width / 3840;
     const verticalScalingFactor = canvas.height / 2160;
