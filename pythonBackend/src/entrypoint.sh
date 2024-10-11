d4781bb455f36c2ac31cdd0d62475b31a27f3e52
marcomenzel <ga58vaf@mytum.de>
2024-10-11 18:05:35 +0200

    Fix Docker

--- pythonBackend/src/entrypoint.sh
+++ pythonBackend/src/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-alembic upgrade head
+export PATH="${PATH}":/backend
+export PYTHONPATH="${PYTHONPATH}":/backend/src
 
-python /src/main.py
+cd /backend
+cd src && alembic upgrade head
+
+#Development Command:
+exec fastapi dev --host 0.0.0.0 --port 80
+
+#Production Command:
+exec fastapi run --host 0.0.0.0 --port 80
--- pythonBackend/src/main.py
+++ pythonBackend/src/main.py
@@ -1,45 +1,49 @@
-from typing import List
+from contextlib import asynccontextmanager
+import os
 import aiofiles
-from fastapi import FastAPI, UploadFile
-
-from .db.db_models import annotations, access_role, game_history, H_key, player, player_history, roles, sport, teams, users, videos
-from .db.database import engine
-from .routers import annotation
 import logging
+from typing import List
+from fastapi import FastAPI, UploadFile
+from .routers import annotation, user
+from alembic.config import Config
+from alembic import command
 
-# Table initialization for each DB model.(replaced by alembic migrations)
-annotations.Base.metadata.create_all(bind=engine)
-access_role.Base.metadata.create_all(bind=engine)
-game_history.Base.metadata.create_all(bind=engine)
-H_key.Base.metadata.create_all(bind=engine)
-player.Base.metadata.create_all(bind=engine)
-player_history.Base.metadata.create_all(bind=engine)
-roles.Base.metadata.create_all(bind=engine)
-sport.Base.metadata.create_all(bind=engine)
-teams.Base.metadata.create_all(bind=engine)
-users.Base.metadata.create_all(bind=engine)
-videos.Base.metadata.create_all(bind=engine)
+log = logging.getLogger('uvicorn')
 
-logger = logging.getLogger('uvicorn.error')
-logger.setLevel(logging.DEBUG)
+def run_migrations():
+    alembic_cfg = Config("alembic.ini")
+    alembic_cfg.set_main_option('script_location', "src/alembic")
+    alembic_cfg.set_main_option('sqlalchemy.url',
+                                os.getenv('DATABASE_URL', 'postgresql://user:pass@localhost:5432/db'))
+    alembic_cfg.set_main_option('prepend_sys_path', '.')
+    command.upgrade(alembic_cfg, 'head')
+
+@asynccontextmanager
+async def lifespan(app_: FastAPI):
+    log.info("Starting up...")
+    log.info("run migration")
+    run_migrations()
+    yield
+    log.info("Shutting down...")
 
 # App Creation
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 # Bind Routers from Router Directory
 app.include_router(annotation.router)
+app.include_router(user.router)
 
 
 @app.get("/")
 async def check_app():
     return {"status": "App Running!"}
 
-@app.post("/upload")
-async def upload(files: List[UploadFile]):
-    for file in files:
-        # Todo: Add correct path for upload items
-        async with aiofiles.open("path", 'wb') as out_file:
-            while content := await file.read(1024):
-                await out_file.write(content)
-
-    return "Files successfully uploaded"
+# @app.post("/upload")
+# async def upload(files: List[UploadFile]):
+#     for file in files:
+#         # Todo: Add correct path for upload items
+#         async with aiofiles.open("path", 'wb') as out_file:
+#             while content := await file.read(1024):
+#                 await out_file.write(content)
+#
+#     return "Files successfully uploaded"
